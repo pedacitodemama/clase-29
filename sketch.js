5a4ecaaa214f93c335ebbe40f7f84d8c7f77cfeb
pedacitodemama <consu8915@gmail.com>
2021-11-28 15:08:36 -0600

    se agregaron los cuerpos restringidos

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint=Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var fondo;
-var plataforma;
+var plataforma, constrainedlog;
 
 function preload(){
     fondo=loadImage("sprites/bg.png")
@@ -18,7 +19,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
-	plataforma=new Ground(150,300,300,170);
+	plataforma=new Ground(150,320,300,170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -34,8 +35,20 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    constrainedlog=new Log(230,180,80,PI/4)
 
     bird = new Bird(100,100);
+
+    var options={
+        bodyA:bird.body,
+        bodyB:constrainedlog.body,
+        stiffness:0.04,
+        length:10
+    }
+    var chain=Constraint.create(options)
+    World.add(world,chain)
+    
+
 
 }
 
@@ -60,4 +73,7 @@ function draw(){
 
     bird.display();
 	plataforma.display();
+    constrainedlog.display();
+    strokeWeight(3);
+    line(bird.body.position.x,bird.body.position.y,constrainedlog.body.position.x,constrainedlog.body.position.y)
 }
